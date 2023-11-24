946854e84086e4d726e83cf04c3c3591a428f743
YutoShinohara <yuto.shino0128@gmail.com>
2023-11-24 23:28:55 +0900

    Initial Commit

---
+++ swiftui_cameraapp_complete/swiftui_cameraapp_completeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_cameraapp_completeApp.swift
+//  swiftui_cameraapp_complete
+//
+//  Created by 篠原優仁 on 2023/11/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_cameraapp_completeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
