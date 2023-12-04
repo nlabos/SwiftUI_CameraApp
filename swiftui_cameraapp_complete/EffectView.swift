7e4b8e4af5b123b2c6df61c4c9962d4213c84bcf
Yuki Takatsu <46854753+yuuki00682200@users.noreply.github.com>
2023-12-04 20:08:42 +0900

    Update EffectView.swift

--- swiftui_cameraapp_complete/EffectView.swift
+++ swiftui_cameraapp_complete/EffectView.swift
@@ -1,198 +1,67 @@
-//
-//  EffectView.swift
-//  swiftui_cameraapp_complete
-//
-//  Created by 篠原優仁 on 2023/11/25.
-//
-
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct EffectView: View {
     @Binding var selectedImage: UIImage?
-    @Binding var processedImage: UIImage?
-
+    
     var body: some View {
+        
+        
         VStack {
-            HStack{
-                Spacer()
-                if let image = processedImage {
-                    Image(uiImage: applyEffect(to: image, effectname: "CIPhotoEffectMono") ?? processedImage!)
-                                .resizable()
-                                .frame(width: 200, height: 200)
-                                .padding()
-                                .onTapGesture {
-                                    // Apply effect to selected image
-                                    processedImage = applyEffect(to: selectedImage, effectname: "CIPhotoEffectMono")
-                                }
-                        } else if let originalImage = selectedImage {
-                            Image(uiImage: originalImage)
-                                .resizable()
-                                .frame(width: 300, height: 200)
-                                .padding()
-                                .onTapGesture {
-                                    // Apply effect to selected image
-                                    processedImage = applyEffect(to: selectedImage, effectname: "CIPhotoEffectMono")
-                                }
-                        } else {
-                            Text("表示する画像を選択してください")
-                        }
-                
-                if let image = processedImage {
-                            Image(uiImage: applyEffect(to: image, effectname: "CIPhotoEffectInstant") ?? processedImage!)
-                                .resizable()
-                                .frame(width: 200, height: 200)
-                                .padding()
-                                .onTapGesture {
-                                    // Apply effect to selected image
-                                    processedImage = applyEffect(to: selectedImage, effectname: "CIPhotoEffectInstant")
-                                }
-                        } else if let originalImage = selectedImage {
-                            Image(uiImage: originalImage)
-                                .resizable()
-                                .frame(width: 200, height: 200)
-                                .padding()
-                                .onTapGesture {
-                                    // Apply effect to selected image
-                                    processedImage = applyEffect(to: selectedImage, effectname: "CIPhotoEffectInstant")
-                                }
-                        } else {
-                            Text("表示する画像を選択してください")
-                        }
-                Spacer()
-                    }
-            HStack{
-                Spacer()
-                if let image = processedImage {
-                            Image(uiImage: applyEffect(to: image, effectname: "CIPhotoEffectProcess") ?? processedImage!)
-                                .resizable()
-                                .frame(width: 200, height: 200)
-                                .padding()
-                                .onTapGesture {
-                                    // Apply effect to selected image
-                                    processedImage = applyEffect(to: selectedImage, effectname: "CIPhotoEffectProcess")
-                                }
-                        } else if let originalImage = selectedImage {
-                            Image(uiImage: originalImage)
-                                .resizable()
-                                .frame(width: 200, height: 200)
-                                .padding()
-                                .onTapGesture {
-                                    // Apply effect to selected image
-                                    processedImage = applyEffect(to: selectedImage, effectname: "CIPhotoEffectProcess")
-                                }
-                        } else {
-                            Text("表示する画像を選択してください")
-                        }
-                        
-                if let image = processedImage {
-                            Image(uiImage: applyEffect(to: image, effectname: "CIPhotoEffectTransfer") ?? processedImage!)
-                                .resizable()
-                                .frame(width: 200, height: 200)
-                                .padding()
-                                .onTapGesture {
-                                    // Apply effect to selected image
-                                    processedImage = applyEffect(to: selectedImage,effectname: "CIPhotoEffectTransfer")
-                                }
-                        } else if let originalImage = selectedImage {
-                            Image(uiImage: originalImage)
-                                .resizable()
-                                .frame(width: 200, height: 200)
-                                .padding()
-                                .onTapGesture {
-                                    // Apply effect to selected image
-                                    processedImage = applyEffect(to: selectedImage,effectname: "CIPhotoEffectTransfer")
-                                }
-                        } else {
-                            Text("表示する画像を選択してください")
-                        }
-                Spacer()
-                    }
-            HStack{
-                Spacer()
-                if let image = processedImage {
-                            Image(uiImage: applyEffect(to: image, effectname: "CISepiaTone") ?? processedImage!)
-                                .resizable()
-                                .frame(width: 200, height: 200)
-                                .padding()
-                                .onTapGesture {
-                                    // Apply effect to selected image
-                                    processedImage = applyEffect(to: selectedImage,effectname: "CISepiaTone")
-                                }
-                        } else if let originalImage = selectedImage {
-                            Image(uiImage: originalImage)
-                                .resizable()
-                                .frame(width: 200, height: 200)
-                                .padding()
-                                .onTapGesture {
-                                    // Apply effect to selected image
-                                    processedImage = applyEffect(to: selectedImage,effectname: "CISepiaTone")
-                                }
-                        } else {
-                            Text("表示する画像を選択してください")
-                        }
-                        
-                if let image = processedImage {
-                            Image(uiImage: applyEffect(to: image, effectname: "CIVignette") ?? processedImage!)
-                                .resizable()
-                                .frame(width: 200, height: 200)
-                                .padding()
-                                .onTapGesture {
-                                    // Apply effect to selected image
-                                    processedImage = applyEffect(to: selectedImage,effectname: "CIVignette")
-                                }
-                        } else if let originalImage = selectedImage {
-                            Image(uiImage: originalImage)
-                                .resizable()
-                                .frame(width: 200, height: 200)
-                                .padding()
-                                .onTapGesture {
-                                    // Apply effect to selected image
-                                    processedImage = applyEffect(to: selectedImage,effectname: "CIVignette")
-                                }
-                        } else {
-                            Text("表示する画像を選択してください")
-                        }
-                    }
-            .padding()
-            .navigationTitle("エフェクトを選択")
+            Text("エフェクトサンプル")
+                .font(.title)
+                .fontWeight(.bold)
+            if let selectedImage = selectedImage {
+                HStack {
+                    EffectImageView(image: selectedImage, effectName: "CIPhotoEffectMono")
+                    EffectImageView(image: selectedImage, effectName: "CIPhotoEffectInstant")
                 }
-                
-    }
-
-    private func applyEffect(to image: UIImage?, effectname: String) -> UIImage? {
-        //エフェクト前の画像ををアンラップして取り出す
-        if let image = selectedImage {
-            //フィルター名を指定する
-            //let filterName = effectname
-            //元々の画像の回転角度を取得
-            let rotate = image.imageOrientation
-            //UIImage型の画像をCIImage型に変換
-            guard let inputImage = CIImage(image: image) else {
-                        return image
-                    }
-            //フィルターに種類を引数で指定されたものに指定してCIFilterインスタンスを取得
-            guard let effectFilter = CIFilter(name: effectname) else {
-                return image
+                HStack {
+                    EffectImageView(image: selectedImage, effectName: "CIPhotoEffectProcess")
+                    EffectImageView(image: selectedImage, effectName: "CIPhotoEffectTransfer")
+                }
+                HStack {
+                    EffectImageView(image: selectedImage, effectName: "CISepiaTone")
+                    EffectImageView(image: selectedImage, effectName: "CIVignette")
+                }
+            } else {
+                Text("画像を選択してください")
             }
-            //エフェクトのパラメータを初期化
-            effectFilter.setDefaults()
-            //インスタンスにエフェクトする元画像を指定
-            effectFilter.setValue(inputImage, forKey: kCIInputImageKey)
-            //エフェクトを適用したCGImage型の画像を取得
-            guard let outputImage = effectFilter.outputImage else {
-                return image
-            }
-            //CIContextのインスタンスを取得
-            let ciContext = CIContext(options: nil)
-            //エフェクト後の画像をCIContext上に描画し、結果をCGImage型で取得
-            guard let cgImage = ciContext.createCGImage(outputImage, from: outputImage.extent) else{
-                return image
-            }
-            
-            let filteredImage = UIImage(cgImage: cgImage, scale: image.scale, orientation: rotate)
-            return filteredImage
         }
-        return image
+        .navigationBarTitle("エフェクトサンプル", displayMode: .inline)
+        
+        
     }
 }
 
+struct EffectImageView: View {
+    var image: UIImage
+    var effectName: String
+    
+    var body: some View {
+        VStack {
+            Image(uiImage: applyEffect(to: image, effectName: effectName) ?? image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150)
+            Text(effectName)
+        }
+    }
+    
+    
+    private func applyEffect(to image: UIImage, effectName: String) -> UIImage? {
+        let context = CIContext()
+        guard let filter = CIFilter(name: effectName) else { return nil }
+        filter.setDefaults()
+        
+        let inputImage = CIImage(image: image)
+        filter.setValue(inputImage, forKey: kCIInputImageKey)
+        
+        if let outputImage = filter.outputImage,
+           let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+            return UIImage(cgImage: cgImage, scale: image.scale, orientation: image.imageOrientation)
+        }
+        return nil
+    }
+}
